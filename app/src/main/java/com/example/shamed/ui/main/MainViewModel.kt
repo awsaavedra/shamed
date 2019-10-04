b4e46125cf86baa1902091bff776e6e3b3845e3c
Alexander Saavedra <awsaavedra@gmail.com>
2019-10-03 23:38:28 -0700

    first go at a skeleton UI

--- app/src/main/java/com/example/shamed/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/shamed/ui/main/MainViewModel.kt
@@ -4,4 +4,10 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    // Save state
+    // TODO: next meeting learn how to persist data (using room)
+        //1) Data inputted by user (assuming all data is mandatory)
+        //2) daily inputs: i.e. breakfast, lunch, dinner (3 saved states for instance)
+        //3)
+
 }
